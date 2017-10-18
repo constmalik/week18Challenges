02f1a85d03bf460534012a590fe53aba19eca34a
Malik Robinson <maliklorenzorobinson@gmail.com>
2017-10-18 14:42:42 -0400

    completed challenge four

--- ChallengeFour/ChallengeFour/Program.cs
+++ ChallengeFour/ChallengeFour/Program.cs
@@ -4,16 +4,27 @@ namespace ChallengeFour
 {
     class Program
     {
-        public class Vehicle 
+        static void Main() 
         {
-            public int tires { get; set; }
-            public string type { get; set; }
-            public int year { get; set; }
-            public string model { get; set; }
-        }
-        public Program(int tires, string type, int year, string model)
-        {
-
+            var DreamCar = new Vehicle(4, "Truck", 2017, "Jeep");
+            var HateCar = new Vehicle(4, "", 2010, "Beetle");
+            Console.WriteLine("My dream car is a" + " " + DreamCar.Year + " " + DreamCar.Model);
+            Console.WriteLine("A car I would hate to own is a" + " " + HateCar.Year + " " + HateCar.Model);
         }
     }
+	public class Vehicle
+	{
+		public int Tires { get; set; }
+		public string Type { get; set; }
+		public int Year { get; set; }
+		public string Model { get; set; }
+
+        public Vehicle(int tires, string type, int year, string model) 
+        {
+            Tires = tires;
+            Type = type;
+            Year = year;
+            Model = model;
+        }
+	}
 }
